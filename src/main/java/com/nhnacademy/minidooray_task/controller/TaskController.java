916f33fa3b51d01c1b035959ecb5edfabc4f43a9
dayea11 <dayea021128@gmail.com>
2026-05-25 20:06:07 +0900

    커밋

--- src/main/java/com/nhnacademy/minidooray_task/controller/TaskController.java
+++ src/main/java/com/nhnacademy/minidooray_task/controller/TaskController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/tasks")
+@RequestMapping("/projects/{projectId}/tasks")
 @RequiredArgsConstructor
 public class TaskController {
 
